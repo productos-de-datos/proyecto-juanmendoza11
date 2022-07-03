5325583999f2f4287a6180d6d897a372f963b28b
Juan Sebastian Mendoza <jmendozaco@unal.edu.co>
2022-07-03 12:55:45 -0500

    Update create_data_lake.py

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -21,7 +21,23 @@ def create_data_lake():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    # raise NotImplementedError("Implementar esta función")
+
+    import os
+    os.mkdir('data_lake')
+    first_layer = ['landing', 'raw', 'cleansed', 'business']
+
+    for i in first_layer:
+        os.mkdir(os.path.join('data_lake', i))
+
+    business_layer =[
+        'business/reports',
+        'business/reports/figures',
+        'business/features',
+        'business/forecasts',]
+
+    for i in business_layer:
+        os.mkdir(os.path.join("data_lake", i))
 
 
 if __name__ == "__main__":
