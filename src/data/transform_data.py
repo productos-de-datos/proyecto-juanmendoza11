68969b57ea50812ae643a4b1b8b107322d73276e
Juan Sebastian Mendoza <jmendozaco@unal.edu.co>
2022-07-03 15:29:25 -0500

    Update transform_data.py

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -9,8 +9,26 @@ def transform_data():
     """
     raise NotImplementedError("Implementar esta función")
 
+    import pandas as pd
+
+    for j in range(1995, 2022):
+        if j in [1995,1999]:
+            df= pd.read_excel('data_lake/landing/{}.xlsx'.format(j), header=3)
+            df.to_csv('data_lake/raw/{}.csv'.format(j), index=None)
+        if j in [2000, 2015]:
+            df = pd.read_excel('data_lake/landing/{}.xlsx'.format(j), header=2)
+            df.to_csv('data_lake/raw/{}.csv'.format(j), index=None)
+        if j in [2016, 2017]:
+            df = pd.read_excel('data_lake/landing/{}.xls'.format(j), header=2)
+            df.to_csv('data_lake/raw/{}.csv'.format(j), index=None)
+        if j in [2018, 2021]:
+            df = pd.read_excel('data_lake/landing/{}.xlsx'.format(j), header=0)
+            df.to_csv('data_lake/raw/{}.csv'.format(j), index=None)
+
+
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    ingest_data()
