fd252a8284a958e85ed1e7e3982387238f3a7055
Juan Sebastian Mendoza <jmendozaco@unal.edu.co>
2022-07-03 22:56:19 -0500

    Update make_daily_prices_plot.py

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -7,10 +7,34 @@ def make_daily_prices_plot():
 
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
     """
-    raise NotImplementedError("Implementar esta función")
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    
+    data = r'data_lake/business/precios-diarios.csv'
+    datos = pd.read_csv(data, index_col=None, sep=',', header=0)
+    datos["fecha"] = pd.to_datetime(datos["fecha"])
+
+    x=datos.fecha
+    y=datos.precio
+
+    plt.figure(figsize=(15, 6))
+    plt.plot(x, y, 'b', label='Promedio diario')
+    plt.title('Promedio diario')
+    plt.xlabel('Fecha')
+    plt.ylabel('Precio')
+    plt.legend()
+    plt.xticks(rotation="vertical")
+    plt.savefig('data_lake/business/reports/figures/daily_prices.png')
+
+    #raise NotImplementedError("Implementar esta función")
+
+
+
+
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    make_daily_prices_plot()
