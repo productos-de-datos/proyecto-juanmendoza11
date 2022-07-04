17aee1e59aae7cf74f8f345086a8477f4fa25af2
Juan Sebastian Mendoza <jmendozaco@unal.edu.co>
2022-07-03 23:13:00 -0500

    -

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -1,19 +1,18 @@
 def make_daily_prices_plot():
-    
     """Crea un grafico de lines que representa los precios promedios diarios.
-
     Usando el archivo data_lake/business/precios-diarios.csv, crea un grafico de
     lines que representa los precios promedios diarios.
-
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
     """
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    
+    df = pd.read_csv('data_lake/business/precios-diarios.csv')
+    plot = df.plot(x='fecha', y = 'precio', kind = 'line').get_figure()
+    plot.savefig('data_lake/business/reports/figures/daily_prices.png')
 
-
+    
     #raise NotImplementedError("Implementar esta función")
-
-
-
-
 
 
 if __name__ == "__main__":
