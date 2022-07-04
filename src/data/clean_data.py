66be346791b9c3609bee16055a439c813bba65a9
Juan Sebastian Mendoza <jmendozaco@unal.edu.co>
2022-07-03 19:01:01 -0500

    -

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -12,10 +12,33 @@ def clean_data():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    import pandas as pd
+    import glob
+
+    ruta = glob.glob(r'data_lake/raw/*.csv')
+
+    for i, archivo in enumerate(ruta):
+        if i == 0:
+            inicial = pd.read_csv(archivo, index_col=None, header=0)
+            hours = inicial.iloc[:, :25]
+            hours.columns = ['fecha']+[('0'+str(i))[-2:] for i in range(24)]
+            inicial_transform = hours.melt(
+                id_vars='fecha', var_name='hora', value_name='precio')
+            final = inicial_transform
+        else:
+            inicial = pd.read_csv(archivo, index_col=None, header=0)
+            hours = inicial.iloc[:, :25]
+            hours.columns = ['Fecha']+[('0'+str(i))[-2:] for i in range(24)]
+            inicial_transform = hours.melt(id_vars='fecha', var_name='hora', value_name='precio')
+            final = pd.concat([final, inicial_transform])
+    final.to_csv('data_lake/cleansed/precios-horarios.csv', index=None)
+
+    
+    
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
-
+    clean_data()
     doctest.testmod()
