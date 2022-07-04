5e84c7b1a056fc41dee431621f1eca213237b3fb
Juan Sebastian Mendoza <jmendozaco@unal.edu.co>
2022-07-04 09:58:11 -0500

    Comentando funciones

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -4,18 +4,15 @@ def make_daily_prices_plot():
     lines que representa los precios promedios diarios.
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
     """
-
     import matplotlib.pyplot as plt
     import pandas as pd
     
-    #Se genera la grafica de precios diarios y se transfiere a la carpeta business.
-    df = pd.read_csv('data_lake/business/precios-diarios.csv') 
-    fig_diarios = df.fig_diarios(x='fecha', y = 'precio', kind = 'line').get_figure()
-    fig_diarios.savefig('data_lake/business/reports/figures/daily_prices.png')
+    # Se crea la grafica de precios diarios y se transfiere a la capa business.
+    df = pd.read_csv('data_lake/business/precios-diarios.csv')
+    plot = df.plot(x='fecha', y = 'precio', kind = 'line').get_figure()
+    plot.savefig('data_lake/business/reports/figures/daily_prices.png')
 
-    
     #raise NotImplementedError("Implementar esta función")
-
 
 if __name__ == "__main__":
     import doctest
