c7b80fa376c4311ede50ff317e6d383295603eec
Juan Sebastian Mendoza <jmendozaco@unal.edu.co>
2022-07-03 20:01:01 -0500

    Create make_monthly_prices_plot.py

---
+++ src/visualization/make_monthly_prices_plot.py
@@ -0,0 +1,21 @@
+def make_monthly_prices_plot():
+    """Crea un grafico de lines que representa los precios promedios mensuales.
+
+    Usando el archivo data_lake/business/precios-mensuales.csv, crea un grafico de
+    lines que representa los precios promedios mensuales.
+
+    El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/monthly_prices.png.
+    """
+    #raise NotImplementedError("Implementar esta función")
+    
+    import pandas as pd
+    from matplotlib import pyplot as plt
+    
+    df = pd.read_csv('data_lake/business/precios-mensuales.csv')
+    monthly_prices_plot = df.plot(x='fecha', y = 'precio', kind = 'line').get_figure()
+    monthly_prices_plot.savefig('data_lake/business/reports/figures/monthly_prices.png')
+
+if __name__ == "__main__":
+    import doctest
+    make_monthly_prices_plott()
+    doctest.testmod()
