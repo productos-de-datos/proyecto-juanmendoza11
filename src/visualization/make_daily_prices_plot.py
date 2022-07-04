bdbc61ccf9c2d7e0f02a5127b5b150eb9ea99fc7
Juan Sebastian Mendoza <jmendozaco@unal.edu.co>
2022-07-03 22:51:11 -0500

    -

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -7,16 +7,10 @@ def make_daily_prices_plot():
 
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
     """
-    #raise NotImplementedError("Implementar esta función")
-    
-    import pandas as pd
-
-    daily_prices = pd.read_csv('data_lake/business/precios-diarios.csv', index_col=None, header=0)
-    daily_prices.plot.line(x='fecha', y='precio').get_figure().savefig('data_lake/business/reports/figures/daily_prices.png')
-#raise NotImplementedError("Implementar esta función")
+    raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
-    make_daily_prices_plot()
+
     doctest.testmod()
--- src/visualization/make_monthly_prices_plot.py
+++ src/visualization/make_monthly_prices_plot.py
@@ -6,16 +6,10 @@ def make_monthly_prices_plot():
 
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/monthly_prices.png.
     """
-    #raise NotImplementedError("Implementar esta función")
+    raise NotImplementedError("Implementar esta función")
     
-    import pandas as pd
-
-    daily_prices = pd.read_csv(
-        'data_lake/business/precios-mensuales.csv', index_col=None, header=0)
-    daily_prices.plot.line(x='Fecha', y='Precio').get_figure().savefig(
-        'data_lake/business/reports/figures/monthly_prices.png')
 
 if __name__ == "__main__":
     import doctest
-    make_monthly_prices_plot()
+
     doctest.testmod()
