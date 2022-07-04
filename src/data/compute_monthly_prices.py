5c3fc11931b27f9077ac282fee2de3fa7ca9dd82
Juan Sebastian Mendoza <jmendozaco@unal.edu.co>
2022-07-03 19:26:43 -0500

    -

--- src/data/compute_monthly_prices.py
+++ src/data/compute_monthly_prices.py
@@ -12,10 +12,18 @@ def compute_monthly_prices():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    #raise NotImplementedError("Implementar esta función")
+    import pandas as pd
+    
+    df = pd.read_csv('data_lake/cleansed/precios-horarios.csv')
+    df['fecha'] = pd.to_datetime(df['fecha'])
+    df = df.set_index('fecha').resample("M")['precio'].mean()
+    df.to_csv('data_lake/business/precios-mensuales.csv', index=True)
+
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
-
+    compute_monthly_prices()
     doctest.testmod()
